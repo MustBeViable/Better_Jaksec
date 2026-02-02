3e4c3be2dc976d9d415ca85032f1bb5d46065916
Elias Rinne <elias1rinne@gmail.com>
2026-02-02 17:47:16 +0200

    add post route for student table and tested it with curl.

--- src/main/java/com/api/student/StudentRepository.java
+++ src/main/java/com/api/student/StudentRepository.java
@@ -1,8 +1,12 @@
 package com.api.student;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
- * Jakarta persistence goes here
+ * Jakarta persistence goes here. Creates interface for repository which lets us use
+ * multiple existong methods like existsByEmailIgoneCase
  */
 
-public class StudentRepository {
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    boolean existsByEmailIgnoreCase(String email);
 }
--- src/main/java/com/api/student/dto/StudentDto.java
+++ src/main/java/com/api/student/dto/StudentDto.java
@@ -1,4 +1,25 @@
 package com.api.student.dto;
 
 public class StudentDto {
+    private int studentID;
+    private String name;
+    private String email;
+
+    public StudentDto(int studentID, String name, String email) {
+        this.studentID = studentID;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
