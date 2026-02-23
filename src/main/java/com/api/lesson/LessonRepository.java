33e248bfb4784f792cc92a7bc21f82d5515c5b9b
leevivl <leevi.laune@metropolia.fi>
2026-02-23 13:43:18 +0200

    add auth to lesson endpoints

--- src/main/java/com/api/lesson/LessonRepository.java
+++ src/main/java/com/api/lesson/LessonRepository.java
@@ -1,6 +1,18 @@
 package com.api.lesson;
 
+import com.api.course.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
+
+    @Query("""
+        SELECT l
+        FROM Lesson l
+        JOIN l.students sl
+        WHERE LOWER(sl.student.email) = LOWER(:email)
+    """)
+    List<Lesson> findLessonsByStudentEmail(String email);
 }
