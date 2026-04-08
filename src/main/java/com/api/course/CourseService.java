b80767033d2049d9bbd3fe9a9a2a62c7db89d70f
Elias Rinne <elias1rinne@gmail.com>
2026-04-08 17:07:39 +0300

    add default language if front doesnt give one

--- src/main/java/com/api/course/CourseService.java
+++ src/main/java/com/api/course/CourseService.java
@@ -47,15 +47,19 @@ public class CourseService {
     @Transactional
     public CourseDto create(CreateCourseRequest request, Auth auth) {
         if(!auth.getRole().equalsIgnoreCase("admin")
-            && !auth.getRole().equalsIgnoreCase("teacher")){
+                && !auth.getRole().equalsIgnoreCase("teacher")){
             throw new UnauthorizedException("Only admins and teachers can create courses");
         }
 
         System.out.println("CourseService.create.auth: " + auth);
         Course course = mapper.toEmptyCourseEntity(request);
 
-        Language language = this.languageRepository.findById(request.getLocale())
-                .orElseThrow(()-> new NotFoundException("Unknown language"));
+        String locale = (request.getLocale() == null || request.getLocale().isBlank())
+                ? "en_US"
+                : request.getLocale();
+
+        Language language = this.languageRepository.findById(locale)
+                .orElseThrow(() -> new NotFoundException("Unknown language"));
 
         course.setLanguage(language);
         language.getCourses().add(course);
